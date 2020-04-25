c49d22ec8aedbdd88ae1f2ed665f2ebe6bcd6591
hajindvlp <harryhajinchung@gmail.com>
2020-04-25 23:37:26 +0900

    Css Selector

--- src/components/booklist.js
+++ src/components/booklist.js
@@ -7,8 +7,8 @@ export default () => {
     return e("div", {id: "book_list"}, 
         ...booklist.map( book => {
             return e("div", {id : "book"}, 
-                e("img", {src: book.image, id : "book_list_image"}),
-                e("p", {id: "book_list_title"}, book.title)
+                e("img", {src: book.image, id : "image"}),
+                e("p", {id: "title"}, book.title)
             )
     }));
 }
