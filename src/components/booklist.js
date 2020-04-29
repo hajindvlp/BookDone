2709addd1886ec2158f62e90642dbb66c7537c92
hajindvlp <harryhajinchung@gmail.com>
2020-04-29 20:26:22 +0900

    More Books

--- src/components/booklist.js
+++ src/components/booklist.js
@@ -6,9 +6,10 @@ export default () => {
   let booklist = getBookList();
   return e("section", {id: "book_list"}, 
     ...booklist.map( book => {
-      return e("div", {id : "book"}, 
+      return e("a", {href: `/book/${book.id}`},
+      e("div", {id : "book"},
         e("img", {src: book.image, id : "image"})
-      )
+      ));
     }),
     e("div", {id: "add"}, 
       e("span" , {id: "text"}, "책 추가하기")
