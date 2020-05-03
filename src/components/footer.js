d26437fb56c480768c7ae1588cb3de87854e0668
hajindvlp <harryhajinchung@gmail.com>
2020-05-03 12:49:33 +0900

    Made With, By

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,6 +2,6 @@ import e from '../element.js';
 
 export default () => {
   return e("footer", {id: "footer"},
-    e("p", {id: "text"}, "Made By Hajindvlp with 💖")    
+    e("p", {id: "text"}, "Made With 💖 By Hajindvlp ")    
   ) 
 } 
